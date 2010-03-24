38c97ac999fec702afa04910306727439b308d10
Antoine Leclair <antoineleclair@gmail.com>
2010-03-24 11:43:58 -0400

    Initial commit.
    Seems to work fine.
    Unit tests pass.
    I use SimpleTest for the unit tests.

---
+++ csv.php
@@ -0,0 +1,165 @@
+<?php
+
+class Csv {
+	// take a CSV line (utf-8 encoded) and returns an array
+    // 'string1,string2,"string3","the ""string4"""' => array('string1', 'string2', 'string3', 'the "string4"')
+    static public function parseString($string) {
+    	$values = array();
+    	$string = str_replace("\r\n", '', $string); // eat the traling new line, if any
+    	if ($string == '') return $values;
+    	$tokens = explode(',', $string);
+    	$count = count($tokens);
+    	for ($i = 0; $i < $count; $i++) {
+    		$token = $tokens[$i];
+    		$len = strlen($token);
+    		$newValue = '';
+    		if ($len > 0 and $token[0] == '"') { // if quoted
+    			$token = substr($token, 1); // remove leading quote
+    			do { // concatenate with next token while incomplete
+					$complete = Csv::_hasEndQuote($token);
+					$token = str_replace('""', '"', $token); // unescape escaped quotes
+					$len = strlen($token);
+					if ($complete) { // if complete
+						$newValue .= substr($token, 0, -1); // remove trailing quote
+					} else { // incomplete, get one more token
+						$newValue .= $token;
+						$newValue .= ',';
+						if ($i == $count - 1) throw new Exception('Illegal unescaped quote.');
+						$token = $tokens[++$i];
+					}
+    			} while (!$complete);
+    			
+    		} else { // unescaped, use token as is
+    			$newValue .= $token;
+    		}
+    		
+    		$values[] = $newValue;
+    	}
+    	return $values;
+    }
+    
+    static public function escapeString($string) {
+    	$string = str_replace('"', '""', $string);
+    	if (strstr($string, '"') or strstr($string, ',')) {
+    		$string = '"'.$string.'"';
+    	}
+    	
+		return $string;
+    }
+    
+	// checks if a string ends with an unescaped quote
+	// 'string"' => true
+	// 'string""' => false
+	// 'string"""' => true
+	static public function _hasEndQuote($token) {
+		$len = strlen($token);
+		if ($len == 0) return false;
+		elseif ($len == 1 and $token == '"') return true;
+		elseif ($len > 1) {
+			while ($len > 1 and $token[$len-1] == '"' and $token[$len-2] == '"') { // there is an escaped quote at the end
+				$len -= 2; // strip the escaped quote at the end
+			}
+			if ($len == 0) return false; // the string was only some escaped quotes
+			elseif ($token[$len-1] == '"') return true; // the last quote was not escaped
+			else return false; // was not ending with an unescaped quote
+		}
+	}
+}
+
+class CsvReader implements Iterator {
+
+	protected $fileHandle = null;
+	protected $position = null;
+	protected $filename = null;
+	protected $currentLine = null;
+	protected $currentArray = null;
+	
+	public function __construct($filename) {
+		$this->fileHandle = fopen($filename, 'r');
+		if (!$this->fileHandle) return;
+		$this->filename = $filename;
+		$this->position = 0;
+		$this->_readLine();
+	}
+	
+	public function __destruct() {
+		$this->close();
+	}
+	
+	// You should not have to call it unless you need to 
+	// explicitly free the file descriptor
+	public function close() {
+		if ($this->fileHandle) {
+			fclose($this->fileHandle);
+			$this->fileHandle = null;
+		}
+	}
+	
+	public function rewind() {
+		if ($this->fileHandle) {
+			$this->position = 0;
+			rewind($this->fileHandle);
+		}
+		
+		$this->_readLine();
+	}
+
+	public function current() {
+		return $this->currentArray;
+	}
+
+    public function key() {
+        return $this->position;
+    }
+
+    public function next() {
+        $this->position++;
+        $this->_readLine();
+    }
+
+    public function valid() {
+        return $this->currentArray !== null;
+    }
+    
+    protected function _readLine() {
+    	if (!feof($this->fileHandle)) $this->currentLine = utf8_encode(fgets($this->fileHandle));
+    	else $this->currentLine = null;
+		if ($this->currentLine != '') $this->currentArray = Csv::parseString($this->currentLine);
+		else $this->currentArray = null;
+    }
+    
+}
+
+class CsvWriter {
+	
+	protected $fileHandle = null;
+	
+	public function __construct($filename, $mode = 'w') {
+		if ($mode != 'w' and $mode != 'a') throw new Exception('CsvWriter only accepts "w" and "a" mode.');
+		$this->fileHandle = fopen($filename, $mode);
+		if (!$this->fileHandle) throw new Exception("Impossible to open file $filename.");
+	}
+	
+	public function __destruct() {
+		$this->close();
+	}
+	
+	public function addLine(array $values) {
+		foreach ($values as $key => $value) {
+			$values[$key] = utf8_decode(Csv::escapeString($value));
+		}
+		$string = implode(',', $values) . "\r\n";
+		fwrite($this->fileHandle, $string);
+	}
+	
+	// You should not have to call it unless you need to flush the
+	// data from the buffer to your file explicitly before the
+	// end of your script
+	public function close() {
+		if ($this->fileHandle) {
+			fclose($this->fileHandle);
+			$this->fileHandle = null;
+		}
+	}
+	
+}
---
+++ tests/csv.test.php
@@ -0,0 +1,204 @@
+<?php
+require_once(dirname(__FILE__) . '/../simpletest/autorun.php');
+require_once(dirname(__FILE__) . '/../csv.php');
+
+class CsvTest extends UnitTestCase {
+    
+    function testParseString_NormalValues() {
+    	$array = Csv::parseString('string1,string2,string3,string4');
+    	$expected = array('string1', 'string2', 'string3', 'string4');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_OneQuoted() {
+    	$array = Csv::parseString('string1,string2,string3,"string4"');
+    	$expected = array('string1', 'string2', 'string3', 'string4');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_OneQuotedWithQuotesInside() {
+    	$array = Csv::parseString('string1,string2,string3,"""string4"""');
+    	$expected = array('string1', 'string2', 'string3', '"string4"');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_OneQuotedWithCommaInside() {
+    	$array = Csv::parseString('string1,string2,string3,"string4,string5"');
+    	$expected = array('string1', 'string2', 'string3', 'string4,string5');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_OneQuotedWithCommaAndQuotesInside() {
+    	$array = Csv::parseString('string1,string2,string3,"""string4"",string5"');
+    	$expected = array('string1', 'string2', 'string3', '"string4",string5');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_SomeQuotedString() {
+    	$array = Csv::parseString('string1,string2,string3,"""string4","string5"');
+    	$expected = array('string1', 'string2', 'string3', '"string4', 'string5');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testParseString_SomeQuotedStringFromExcel() {
+    	$array = Csv::parseString('One,Two words,"One ""quoted""","Single ""quote"');
+    	$expected = array('One', 'Two words', 'One "quoted"', 'Single "quote');
+    	$this->assertEqual($array, $expected);
+    }
+    
+    function testEscapeString_NormalString() {
+    	$result = Csv::escapeString('a string');
+    	$expected = 'a string';
+    	$this->assertEqual($result, $expected);
+    }
+    
+    function testEscapeString_StringWithComa() {
+    	$result = Csv::escapeString('a string, with comma');
+    	$expected = '"a string, with comma"';
+    	$this->assertEqual($result, $expected);
+    }
+    
+    function testEscapeString_StringWithQuote() {
+    	$result = Csv::escapeString('a string" with quote');
+    	$expected = '"a string"" with quote"';
+    	$this->assertEqual($result, $expected);
+    }
+    
+    function testEscapeString_StringWithQuoteAndComma() {
+    	$result = Csv::escapeString('a string", with quote');
+    	$expected = '"a string"", with quote"';
+    	$this->assertEqual($result, $expected);
+    }
+    
+    function testhasEndQuote_NoQuoteString() {
+    	$result = Csv::_hasEndQuote('a string');
+    	$this->assertFalse($result);
+    }
+    
+    function testhasEndQuote_EndWithSingleQuote() {
+    	$result = Csv::_hasEndQuote('a string"');
+    	$this->assertTrue($result);
+    }
+    
+    function testHasEndQuote_EndWithOneEscapedQuote() {
+    	$result = Csv::_hasEndQuote('a string""');
+    	$this->assertFalse($result);
+    }
+    
+    function testHasEndQuote_EndWithOneEscapedQuoteAndOneNotEscaped() {
+    	$result = Csv::_hasEndQuote('a string"""');
+    	$this->assertTrue($result);
+    }
+    
+    function testHasEndQuote_EmptyString() {
+    	$result = Csv::_hasEndQuote('');
+    	$this->assertFalse($result);
+    }
+    
+    function testHasEndQuote_OnlyOneQuote() {
+    	$result = Csv::_hasEndQuote('"');
+    	$this->assertTrue($result);
+    }
+    
+    function testHasEndQuote_OnlyOneEscapedQuote() {
+    	$result = Csv::_hasEndQuote('""');
+    	$this->assertFalse($result);
+    }
+    
+    function testHasEndQuote_OneEscapedQuoteAndOneUnescaped() {
+    	$result = Csv::_hasEndQuote('"""');
+    	$this->assertTrue($result);
+    }
+    
+    function testHasEndQuote_EndWithOneUnescapedQuoteWithQuotesInside() {
+    	$result = Csv::_hasEndQuote('a strin"g"');
+    	$this->assertTrue($result);
+    }
+    
+    function testHasEndQuote_EndWithOneEscapedQuoteWithQuotesInside() {
+    	$result = Csv::_hasEndQuote('a strin"g""');
+    	$this->assertFalse($result);
+    }
+    
+    
+}
+
+class CsvReaderTest extends UnitTestCase {
+	
+	function testCsvReader_OneLine() {
+    	$lines = new CsvReader(dirname(__FILE__) . '/data/one-line.csv');
+    	foreach ($lines as $line) {
+    		$expected = array('One', 'Two words', 'One "quoted"', 'Single "quote');
+    		$this->assertEqual($line, $expected);
+    	}
+    }
+    
+    function testCsvReader_OneLineWithAccents() {
+    	$lines = new CsvReader(dirname(__FILE__) . '/data/one-line-with-accents.csv');
+    	foreach ($lines as $line) {
+    		$expected = array('One', 'Twô wördç', 'One "quoted"', 'Single "quote');
+    		//echo '<pre>';print_r($line);echo '</pre>';
+    		$this->assertEqual($line, $expected);
+    	}
+    }
+    
+    function testCsvReader_TwoLines() {
+    	$lines = new CsvReader(dirname(__FILE__) . '/data/two-lines.csv');
+    	$values = array();
+    	foreach ($lines as $line) {
+    		$values[] = $line;
+    	}
+		$expected = array(
+			array('One', 'Two words', 'One "quoted"', 'Single "quote'),
+			array('Line Number', '"Two "" is here"', 'Is', 'It fine?')
+		);
+		$this->assertEqual($values, $expected);
+    }
+}
+
+class CsvWriterTest extends UnitTestCase {
+	
+	function testCsvReader_OneLine() {
+    	$filename = dirname(__FILE__) . '/data/writer-one-line.csv';
+    	$csv = new CsvWriter($filename);
+    	$csv->addLine(array('One', 'Two words', 'One "quoted"', 'Single "quote'));
+    	$csv->close();
+    	$file = fopen(dirname(__FILE__) . '/data/writer-one-line.csv', 'r');
+    	$string = fgets($file);
+    	fclose($file);
+    	@unlink($filename);
+    	$expected = 'One,Two words,"One ""quoted""","Single ""quote"'."\r\n";
+    	$this->assertEqual($string, $expected);
+    }
+    
+    function testCsvReader_OneLineWithAccents() {
+    	$filename = dirname(__FILE__) . '/data/writer-one-line.csv';
+    	$csv = new CsvWriter($filename);
+    	$csv->addLine(array('One', 'Twô wördç', 'One "quoted"', 'Single "quote'));
+    	$csv->close();
+    	$file = fopen(dirname(__FILE__) . '/data/writer-one-line.csv', 'r');
+    	$string = utf8_encode(fgets($file));
+    	fclose($file);
+    	@unlink($filename);
+    	$expected = 'One,Twô wördç,"One ""quoted""","Single ""quote"'."\r\n";
+    	$this->assertEqual($string, $expected);
+    }
+    
+    function testCsvReader_TwoLines() {
+    	$filename = dirname(__FILE__) . '/data/writer-one-line.csv';
+    	$csv = new CsvWriter($filename);
+    	$csv->addLine(array('One', 'Two words', 'One "quoted"', 'Single "quote'));
+    	$csv->addLine(array('Line Number', '"Two "" is here"', 'Is', 'It fine?'));
+    	$csv->close();
+    	$file = fopen(dirname(__FILE__) . '/data/writer-one-line.csv', 'r');
+    	$string1 = utf8_encode(fgets($file));
+    	$string2 = utf8_encode(fgets($file));
+    	fclose($file);
+    	@unlink($filename);
+    	$expected1 = 'One,Two words,"One ""quoted""","Single ""quote"'."\r\n";
+    	$expected2 = 'Line Number,"""Two """" is here""",Is,It fine?'."\r\n";
+    	$this->assertEqual($string, $expected);
+    } 
+
+}
+?>
